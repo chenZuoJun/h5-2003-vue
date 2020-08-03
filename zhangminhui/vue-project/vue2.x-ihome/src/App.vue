3dadb8ebf6dc91060bd70c152b4e9728e7941d3a
chenzuojun <1185103853@qq.com>
2020-08-03 21:53:33 +0800

    diyitian

--- zhangminhui/vue-project/vue2.x-ihome/src/App.vue
+++ zhangminhui/vue-project/vue2.x-ihome/src/App.vue
@@ -1,32 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Header></Header>
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Header from '@/components/header.vue'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  name: 'Home',
+  components: {
+    Header
   }
 }
+</script>
+
+<style lang="less" scoped>
+
 </style>
